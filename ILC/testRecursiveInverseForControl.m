15f886b229d41ca27f6894344712797e47c73bc7
Okan Koc <okoc@sill.is.localnet>
2017-08-01 17:48:02 +0200

    Testing the idea that maximum principle should solve for big lifted matrix inversion, i.e. some inv(F). Can it be used to implement recursive, or incremental inverse?

---
+++ ILC/testRecursiveInverseForControl.m
@@ -0,0 +1,53 @@
+%% Testing batch inversion vs. recursive implementation
+
+clc; clear; close all; rng(3);
+
+% Testing the idea that minimum principle for trajectory tracking
+% with quadratic tracking error costs and zero control input penalty
+% gives the same results as batch lifted-matrix inversion
+
+n = 2; % dim_x
+m = 2; % dim_u
+T = 1.0; % final time
+N = 2; % num of traj. points
+dt = T/N; % discretization
+A = randn(n);
+B = randn(n,m);
+As = repmat(A,1,1,N);
+Bs = repmat(B,1,1,N);
+%A = randn(n,n,N);
+%B = randn(n,m,N);
+
+% Discretize and lift nominal  dynamics
+[Ad,Bd] = discretizeDyn(As,Bs,dt);
+F = liftDyn(Ad,Bd);
+
+%% sample reference from a Gaussian Process
+
+hp.type = 'squared exponential ard';
+hp.l = 0.1;
+hp.scale = 1;
+hp.noise.var = 0.01;
+t = dt * (1:N);
+l = hp.l;
+s = hp.scale;
+mu = zeros(N,n);
+kern = @(x1,x2) s * exp(-(norm(x1(:)-x2(:),2)^2)/(2*(l^2)));
+Sigma = ker_matrix(t,kern);
+[U,S] = eig(Sigma);
+r = mu + U * sqrt(max(0,real(S))) * randn(N,n);
+
+%plot(x,r);
+
+%% Invert the reference to find best controls and evolve system from x0=0
+
+rr = r';
+r_lift = rr(:);
+u_lift = F \ r_lift;
+x_lift = F * u_lift;
+x = reshape(x_lift,n,N);
+plot(t,x,t,r);
+disp('Max tracking error:');
+max(max(x - r'))
+disp('Due to inversion error:');
+norm(eye(N*n) - F*pinv(F), 2)
