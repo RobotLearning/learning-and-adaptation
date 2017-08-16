2cf0f86443bb84473e8b47f66bb82d3e50e78cee
Okan Koc <okoc@blies.is.localnet>
2017-08-16 20:32:27 +0200

    Modified n to m

--- ILC/testRecursiveInverseForControl.m
+++ ILC/testRecursiveInverseForControl.m
@@ -6,20 +6,17 @@ clc; clear; close all; rng(3);
 % with quadratic tracking error costs and small control input penalty
 % gives the same results as batch lifted-matrix inversion as R \to 0
 
-n = 2; % dim_x
-m = 2; % dim_u
+n = 3; % dim_x
+m = 1; % dim_u
 T = 1.0; % final time
 N = 50; % num of traj. points
 dt = T/N; % discretization
-A = randn(n);
-B = randn(n,m);
+A = randn(n); %[0 1 0; 0 0 1; -4 -6 -4];
+B = randn(n,m); %[0; 0; 1];
 As = repmat(A,1,1,N);
 Bs = repmat(B,1,1,N);
 %A = randn(n,n,N);
 %B = randn(n,m,N);
-
-% Discretize and lift nominal dynamics
-[Ad,Bd] = discretizeDyn(As,Bs,dt);
 
 %% sample reference from a Gaussian Process
 
@@ -45,10 +42,17 @@ r = r(2:end,:);
 
 rr = r';
 r_lift = rr(:);
-F = liftDyn(Ad,Bd);
+% Discretize and lift nominal dynamics
+tic; 
+[Ad,Bd] = discretizeDyn(As,Bs,dt); 
+toc
+F = liftDyn(Ad,Bd); % seems this operation is costly
+toc
+tic
 u_lift = F \ r_lift;
+fprintf('Plant inversion took %f sec.\n', toc);
 x_lift = F * u_lift;
-u_invert = reshape(u_lift,n,N);
+u_invert = reshape(u_lift,m,N);
 x_invert = reshape(x_lift,n,N);
 figure(1);
 plot(t,x_invert,t,r);
@@ -61,22 +65,22 @@ max(max(abs(u_invert)))
 
 %% Solve u's instead with BVP (minimum principle)
 
+tic;
 solinit = bvpinit(linspace(dt,T,N),zeros(1,2*n));
-
 params.A = A;
 params.B = B;
 params.Q = eye(n);
 params.R = 0.0001*eye(m);
 params.ref = r;
 params.ts = t;
-
 odefun = @(t,x) pmp_ltv_ref(t,x,params);
 bcfun = @(xa,xb) [xa(1:n)-rr(:,1); xb(n+1:end);];
 sol = bvp4c(odefun,bcfun,solinit);
 sol_at_t = deval(sol,t);
-x_incr = sol_at_t(1:2,:);
-lambda = sol_at_t(3:4,:);
+x_incr = sol_at_t(1:n,:);
+lambda = sol_at_t(n+1:end,:); % TODO: should be evaluated from 0 to T-dt
 u_incr = -params.R\(B'*lambda);
+fprintf('Stable (incr.) inversion took %f sec.\n', toc);
 figure(2);
 plot(t,x_incr,t,r);
 disp('Max tracking error:');
